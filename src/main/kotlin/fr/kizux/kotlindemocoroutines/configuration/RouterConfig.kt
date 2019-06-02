5750461675b74125c55386ad9cbf0a81cf1814ea
kizux <orlyne@kizux.fr>
2019-06-02 19:09:53 +0200

    Add HTTP headers checks

--- src/main/kotlin/fr/kizux/kotlindemocoroutines/configuration/RouterConfig.kt
+++ src/main/kotlin/fr/kizux/kotlindemocoroutines/configuration/RouterConfig.kt
@@ -3,6 +3,7 @@ package fr.kizux.kotlindemocoroutines.configuration
 import fr.kizux.kotlindemocoroutines.handler.UserHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.server.coRouter
 
 @Configuration
@@ -10,10 +11,10 @@ class RouterConfig {
 
     @Bean
     fun userRoutes(userHandler: UserHandler) = coRouter {
-        "/user".nest {
+        "/user".and(accept(MediaType.APPLICATION_JSON)).nest {
             GET("", userHandler::getAll)
             GET("/{id}", userHandler::get)
-            POST("", userHandler::create)
+            POST("").and(contentType(MediaType.APPLICATION_JSON)).invoke(userHandler::create)
         }
     }
 }
